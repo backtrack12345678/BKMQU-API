cb883c92587e4a832be631aed58ba78b266dcb8f
backtrack12345678 <41798969+backtrack12345678@users.noreply.github.com>
2024-08-09 13:51:21 +0700

    will delete soon

--- src/common/pipes/file-validator.ts
+++ src/common/pipes/file-validator.ts
@@ -1,11 +1,15 @@
 import { BadRequestException } from '@nestjs/common';
+import { fromFile } from 'file-type';
 
-export function validateFileType(
+export async function validateFileType(
   allowedMimeTypes: Record<string, string[]>,
   file: Express.Multer.File | any,
   cb: (error: Error | null, acceptFile: boolean) => void,
-): void {
-  allowedMimeTypes[file.fieldname].includes(file.mimetype)
+): Promise<void> {
+  console.log(file);
+
+  const { mime } = await fromFile(file.path);
+  allowedMimeTypes[file.fieldname].includes(mime)
     ? cb(null, true)
     : cb(
         new BadRequestException(`File type for ${file.fieldname} not allowed`),
