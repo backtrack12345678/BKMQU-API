30d583ad0b1df9cc1a658504120b8751621f79cd
backtrack12345678 <41798969+backtrack12345678@users.noreply.github.com>
2024-08-15 15:52:35 +0700

    fix aktivitas notification

--- src/common/notification/notification.service.ts
+++ src/common/notification/notification.service.ts
@@ -14,19 +14,22 @@ export class NotificationService {
     try {
       for (let i = 0; i < requestCount; i++) {
         const batch = jamaahNotifToken.slice(i * batchSize, (i + 1) * batchSize);
-        const tes = await this.axiosService.notificationInstance.post('--/api/v2/push/send', [
-          {
-            to: batch, // Menggunakan batch yang benar
-            title: `${options.mesjid} Membuat Aktivitas Baru`,
-            body: 'Ketuk Untuk Melihat',
-            priority: 'default',
-            data: {
-              id: options.aktivitasId // aktivitas id
+        const tes = await this.axiosService.notificationInstance.post(
+          '--/api/v2/push/send', 
+          [
+            {
+              to: batch, // Menggunakan batch yang benar
+              title: `${options.mesjid} Membuat Aktivitas Baru`,
+              body: 'Ketuk Untuk Melihat',
+              priority: 'high',
+              data: {
+                id: options.aktivitasId // aktivitas id
+              }
             }
-          }
         ]);
-        
       }
+      
+      
     } catch (e) {
       console.log(e);
       throw new HttpException('Gagal Mengirim Notifikasi', 500);
